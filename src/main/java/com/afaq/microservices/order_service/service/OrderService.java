72b77c0fe625493d34f6a4df132ed82f2b70289d
Afaq Ahmed Khan <afaq.qwe@gmail.com>
2024-09-22 17:56:48 +0200

    Adding openfeign

--- src/main/java/com/afaq/microservices/order_service/service/OrderService.java
+++ src/main/java/com/afaq/microservices/order_service/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.afaq.microservices.order_service.service;
 
+import client.InventoryClient;
 import com.afaq.microservices.order_service.dto.OrderRequest;
 import com.afaq.microservices.order_service.model.Order;
 import com.afaq.microservices.order_service.repository.OrderRepository;
@@ -13,15 +14,22 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest){
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setQuantity(orderRequest.quantity());
-        order.setSkuCode(orderRequest.skuCode());
 
-        orderRepository.save(order);
+        boolean isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if(isProductInStock){
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setQuantity(orderRequest.quantity());
+            order.setSkuCode(orderRequest.skuCode());
+            orderRepository.save(order);
+        }else {
+            throw new RuntimeException("Product with skuCode "+ orderRequest.skuCode() + "is not in stock");
+        }
+
 
     }
 }
